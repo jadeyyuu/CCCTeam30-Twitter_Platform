f42f910090ad3efbf28d3d1fbce1904bf3dbac36
Jade <jadeyyuu@gmail.com>
2021-05-07 20:55:04 +0800

    update inventory file

--- Ansible/run-nectar.sh
+++
@@ -1,6 +0,0 @@
-
-# set up deploy instance
-. ./CCC-grp-30-openrc.sh; ansible-playbook deploy_instance.yaml
-
-# set up environment 
-. ./CCC-grp-30-openrc.sh; ansible-playbook --ask-become-pass set_environment.yaml
