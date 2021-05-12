2446fff3fd4d203fdb9cb785e897c86eb0403a6c
Jade <jadeyyuu@gmail.com>
2021-05-12 23:37:12 +0800

    update couch DB

--- Ansible/deploy_applications.sh
+++ Ansible/deploy_applications.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export ANSIBLE_HOST_KEY_CHECKING=False
-. ./group7_openrc.sh; ansible-playbook --ask-become-pass deploy_applications.yaml -i inventory/hosts.ini
+. ./CCC-grp-30-openrc.sh; ansible-playbook --ask-become-pass deploy_applications.yaml -i inventory/hosts.ini
--- Ansible/deploy_db.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-. ./CCC-grp-30-openrc.sh; ansible-playbook --ask-become-pass deploy_db.yaml -i inventory/hosts.ini
